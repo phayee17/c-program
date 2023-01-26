bcabac463d25fe9507a7662dce18b9cb24dd01e3
phayee17 <123065450+phayee17@users.noreply.github.com>
2023-01-26 13:26:11 +0300

    Add files via upload

---
+++ product.c
@@ -0,0 +1,14 @@
+//program to find the product of two numbers
+#include <stdio.h>
+int main()
+{
+  int x,y,product;
+  printf("\n Enter value of X:");
+  scanf("%d",&x);
+  printf("\n Enter value of Y:");
+  scanf("%d",&y);
+  product=x*y;
+  printf("\n PRODUCT=%d",product);
+  return 0;
+  }
+    
